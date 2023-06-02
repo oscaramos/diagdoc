4b67a36ceae133177ca6b9e149f7f3bbd4f7a71f
Oscar <odrr.99@gmail.com>
2023-06-02 17:41:07 -0500

    feat: testing with two routes open graph

---
+++ src/pages/[diagramType].tsx
@@ -0,0 +1,57 @@
+import { NextSeo } from 'next-seo';
+import { useRouter } from "next/router";
+import { OpenGraph } from "next-seo/lib/types";
+
+const diagramTypes = ["plantuml_use-case", "nomnoml_class"]
+type DiagramType = (typeof diagramTypes)[number]
+
+export default function Page() {
+  const router = useRouter();
+  const diagramType = router.query.diagramType as DiagramType
+
+  const getOpenGraph: Record<DiagramType, OpenGraph> = {
+    "plantuml_use-case": {
+      title: 'Plantuml use-case documentation',
+      description: 'Documentation for creating a use-case diagram in plantuml',
+      type: 'website',
+      images: [
+        {
+          url: '/images/plantuml-usecase.png',
+          width: 820,
+          height: 836,
+          alt: 'Plantuml use-case diagram example',
+        }
+      ],
+    },
+    "nomnoml_class": {
+      title: 'Nomnoml documentation',
+      description: 'Documentation for creating a class diagram in nomnoml',
+      type: 'website',
+      images: [
+        {
+          url: '/images/nomnoml.png',
+          width: 820,
+          height: 651,
+          alt: 'Nomnoml class diagram example',
+        }
+      ]
+    }
+  }
+
+  const openGraph = getOpenGraph[diagramType]
+
+  if(openGraph) {
+    return (
+      <div>
+        <NextSeo
+          openGraph={openGraph}
+        />
+
+        Exist!
+      </div>
+    )
+  }
+  return <div>
+    Does not exist
+  </div>
+}
