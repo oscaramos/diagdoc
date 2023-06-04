81adf9d9d3c4c5062fb365e09bac46ab7f56c901
Oscar <odrr.99@gmail.com>
2023-06-04 00:58:36 -0500

    feat: setup the redirect to documentation links

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// next.config.js
 
-module.exports = nextConfig
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/nomnoml_class",
+        destination: "https://www.nomnoml.com",
+        permanent: true,
+      },
+      {
+        source: "/plantuml_use-case",
+        destination: "https://plantuml.com/use-case-diagram",
+        permanent: true,
+      },
+    ];
+  },
+};
